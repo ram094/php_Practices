bc90cee863bed1cbb90596d99004b092bed39541
Ram Kharel <ram.kharel@mavs.uta.edu>
2019-09-27 10:16:11 -0500

    added a sentence

--- learnphp.php
+++ learnphp.php
@@ -16,15 +16,21 @@
       echo "<p>Data processed </> </b>";
       date_default_timezone_set('America/Chicago');
       echo date(' h:i:s A l F jS Y e '); */
-      
+
         $usersName = $_POST['username']; //passing information
         $streetAddress = $_POST['streetaddress'];
         $cityAddress = $_POST['cityaddress'];
 
 
       echo $usersName . "</br>";
-      echo $streetAddress . "</br>";
-      echo $cityAddress . "</br>";
+      echo $streetAddress . ", ";
+      echo $cityAddress . "." . "</br>" . "</br>";
+
+      $str = <<<EOD
+      The customers name is $usersName and They live at $streetAddress
+      in $cityAddress.
+      EOD;
+      echo $str;
 
     ?>
   </body>
